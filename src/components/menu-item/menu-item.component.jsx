ae4263597285c1db2c9db615a13a2c5777d0c55d
chimmpa <lukusw@googlemail.com>
2021-04-29 14:04:00 +0100

    completed lesson 3

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,14 +4,17 @@ import './menu-item.styles.scss';
 
 function MenuItem({title, imageUrl, size}){
     return(
-        <div 
-        style={{
-            backgroundImage: `url(${imageUrl})`
-        }}
+        <div
         className={`${size} menu-item`}
         >
+            <div 
+                className='background-image'
+                style={{
+                    backgroundImage: `url(${imageUrl})`
+                }}
+            />
             <div className='content'>
-                <h1 className='title'>{title}</h1>
+                <h1 className='title'>{title.toUpperCase()}</h1>
                 <span className='subtitle'>SHOP NOW</span>
             </div>
         </div>
